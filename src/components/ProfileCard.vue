49b2585d153d7443910fe457fb9a96fe65632c52
George Rae <george.rae@28b.co.uk>
2023-06-19 16:59:15 +0100

    ci: create the starting structure

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="card">
+    <p class="card__name">Name: Greg</p>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use '@/assets/style/vars' as *;
+.card {
+  width: clamp(300px, 75%, 600px);
+  padding: $spacing-md;
+
+  font-size: 18px;
+
+  border-radius: $spacing-sm;
+  background: $colour-grey-70;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="profile">
+    <section class="profile__info">
+      <h1 class="profile__title">Greg's Spotify Profile</h1>
+      <p class="profile__description">
+        This is just a nice sample description to be used to fill space lol
+      </p>
+    </section>
+    <ProfileCard />
   </main>
 </template>
+
+<script setup lang="ts">
+import ProfileCard from '@/components/ProfileCard.vue'
+</script>
+
+<style lang="scss" scoped>
+@use '@/assets/style/vars' as *;
+
+.profile {
+  @include flex-x(center, center);
+  width: 100%;
+  height: 600px;
+  gap: $spacing-xl;
+
+  color: $colour-grey-20;
+
+  &__title {
+    margin-bottom: 24px;
+
+    font-size: 48px;
+    font-weight: 700;
+  }
+}
+</style>
